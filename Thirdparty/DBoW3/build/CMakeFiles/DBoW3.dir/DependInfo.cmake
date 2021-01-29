708f3c984da38550f27b1fc1ba6c60ffe3c012b5
Kasper-Borzdynski <61881029+Kasper-Borzdynski@users.noreply.github.com>
2021-01-29 19:58:57 +0100

    Add files via upload

---
+++ Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/quicklz.c" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/quicklz.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/BowVector.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/BowVector.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/Database.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/Database.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/DescManip.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/DescManip.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/FeatureVector.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/FeatureVector.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/QueryResults.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/QueryResults.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/ScoringObject.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/ScoringObject.cpp.o"
+  "/home/kasper/slam3vsp/Thirdparty/DBoW3/src/Vocabulary.cpp" "/home/kasper/slam3vsp/Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/src/Vocabulary.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DBoW3.dir/src/BowVector.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/Database.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/DescManip.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/QueryResults.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/quicklz.c.o"
+  "CMakeFiles/DBoW3.dir/src/FeatureVector.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/ScoringObject.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/Vocabulary.cpp.o"
+  "../lib/libDBoW3.pdb"
+  "../lib/libDBoW3.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/DBoW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
